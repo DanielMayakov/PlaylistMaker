867570b5f53594aec90a18b2b59b488a42352c29
DanielMayakov <80329797+DanielMayakov27@users.noreply.github.com>
2023-12-13 18:52:11 +0300

    sprint 10

--- app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
@@ -2,10 +2,15 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.playlistmaker.databinding.ActivityMediaLibraryBinding
 
 class MediaLibraryActivity : AppCompatActivity() {
+
+    private lateinit var mediaBinding: ActivityMediaLibraryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_library)
+        mediaBinding = ActivityMediaLibraryBinding.inflate(layoutInflater)
+        setContentView(mediaBinding.root)
     }
 }
